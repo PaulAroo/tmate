df6920ad8241ab4574c40616a791d11c5477ee55
PaulAroo <timmypaul2006@gmail.com>
2024-01-18 13:28:49 +0100

    chore: extract types into a single file

--- lib/store.ts
+++ lib/store.ts
@@ -1,35 +1,6 @@
 import { create } from "zustand"
 import { v4 as uuid } from "uuid"
-
-export type Status = "PENDING" | "IN_PROGRESS" | "COMPLETED"
-
-export type Task = {
-	id: string
-	title: string
-	description: string
-	status: Status
-	due_date: Date
-}
-
-interface AddedTaskDetails {
-	title: string
-	description: string
-	due_date: Date
-}
-interface UpdatedTaskDetails {
-	title: string
-	status: Status
-	due_date: Date
-}
-
-export type State = {
-	tasks: Task[]
-}
-
-export type Actions = {
-	addTask: (details: AddedTaskDetails) => void
-	updateTask: (id: string, details: UpdatedTaskDetails) => void
-}
+import { Actions, State } from "./types"
 
 export const useTaskStore = create<State & Actions>()((set) => ({
 	tasks: [],
---
+++ lib/types.ts
@@ -0,0 +1,29 @@
+export type Status = "PENDING" | "IN_PROGRESS" | "COMPLETED"
+
+export type Task = {
+	id: string
+	title: string
+	description: string
+	status: Status
+	due_date: Date
+}
+
+interface AddedTaskDetails {
+	title: string
+	description: string
+	due_date: Date
+}
+interface UpdatedTaskDetails {
+	title: string
+	status: Status
+	due_date: Date
+}
+
+export type State = {
+	tasks: Task[]
+}
+
+export type Actions = {
+	addTask: (details: AddedTaskDetails) => void
+	updateTask: (id: string, details: UpdatedTaskDetails) => void
+}
