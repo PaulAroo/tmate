debffdf563d270cb5f1fff934601b89f8e504244
PaulAroo <timmypaul2006@gmail.com>
2024-02-14 21:16:20 +0100

    feat: use indexdb as persist storage

--- lib/store.ts
+++ lib/store.ts
@@ -1,9 +1,28 @@
 import { create } from "zustand"
 import { v4 as uuid } from "uuid"
-import { persist } from "zustand/middleware"
+import localForage from "localforage"
+import { StateStorage, createJSONStorage, persist } from "zustand/middleware"
 
 import { seedTasks } from "./seed-data"
 import { Actions, State } from "./types"
+
+localForage.config({
+	name: "tmate",
+	storeName: "task_store_1",
+	description: "store a list of tasks",
+})
+
+const storage: StateStorage = {
+	async getItem(name) {
+		return await localForage.getItem(name)
+	},
+	async setItem(name, value) {
+		await localForage.setItem(name, value)
+	},
+	async removeItem(name) {
+		await localForage.removeItem(name)
+	},
+}
 
 export const useTaskStore = create<State & Actions>()(
 	persist(
@@ -30,6 +49,10 @@ export const useTaskStore = create<State & Actions>()(
 				}))
 			},
 		}),
-		{ name: "task-store", skipHydration: true }
+		{
+			name: "task-store",
+			skipHydration: true,
+			storage: createJSONStorage(() => storage),
+		}
 	)
 )
