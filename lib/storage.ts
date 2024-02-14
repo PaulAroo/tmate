1d03d6981ca19ae716744f625fbb06bb3908d6fb
PaulAroo <timmypaul2006@gmail.com>
2024-02-14 22:22:08 +0100

    refactor: custom storage

---
+++ lib/storage.ts
@@ -0,0 +1,20 @@
+import localForage from "localforage"
+import { StateStorage } from "zustand/middleware"
+
+localForage.config({
+	name: "tmate",
+	storeName: "task_store_1",
+	description: "store a list of tasks",
+})
+
+export const storage: StateStorage = {
+	async getItem(name) {
+		return await localForage.getItem(name)
+	},
+	async setItem(name, value) {
+		await localForage.setItem(name, value)
+	},
+	async removeItem(name) {
+		await localForage.removeItem(name)
+	},
+}
--- lib/store.ts
+++ lib/store.ts
@@ -1,28 +1,10 @@
 import { create } from "zustand"
 import { v4 as uuid } from "uuid"
-import localForage from "localforage"
-import { StateStorage, createJSONStorage, persist } from "zustand/middleware"
+import { createJSONStorage, persist } from "zustand/middleware"
 
+import { storage } from "./storage"
 import { seedTasks } from "./seed-data"
 import { Actions, State } from "./types"
-
-localForage.config({
-	name: "tmate",
-	storeName: "task_store_1",
-	description: "store a list of tasks",
-})
-
-const storage: StateStorage = {
-	async getItem(name) {
-		return await localForage.getItem(name)
-	},
-	async setItem(name, value) {
-		await localForage.setItem(name, value)
-	},
-	async removeItem(name) {
-		await localForage.removeItem(name)
-	},
-}
 
 export const useTaskStore = create<State & Actions>()(
 	persist(
