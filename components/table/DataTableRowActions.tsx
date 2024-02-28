5ba573c1c035b42eb569a17bef607251c1a163c0
PaulAroo <timmypaul2006@gmail.com>
2024-02-28 17:34:39 +0100

    refactor: data table actions

--- components/table/DataTableRowActions.tsx
+++ components/table/DataTableRowActions.tsx
@@ -1,18 +1,7 @@
-"use client"
-
-import { useState } from "react"
+import { Trash2 } from "lucide-react"
 import { Row } from "@tanstack/react-table"
-import { DotsHorizontalIcon } from "@radix-ui/react-icons"
 
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import { Task } from "@/store/types"
-import { EditTask } from "./EditTask"
 import { useTaskStore } from "@/store"
 import { Button } from "@/components/ui/button"
 
@@ -26,45 +15,15 @@ export function DataTableRowActions<TData>({
 	const task = row.original as Task
 	const deleteTask = useTaskStore((state) => state.deleteTask)
 
-	const [open, setOpen] = useState(false)
-
 	return (
-		<>
-			<DropdownMenu>
-				<DropdownMenuTrigger asChild>
-					<Button
-						variant="ghost"
-						className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-					>
-						<DotsHorizontalIcon className="h-4 w-4" />
-						<span className="sr-only">Open menu</span>
-					</Button>
-				</DropdownMenuTrigger>
-				<DropdownMenuContent align="end" className="w-[100px]">
-					<DropdownMenuItem className="p-0">
-						<Button
-							variant="ghost"
-							className="justify-start py-0 px-2 w-full"
-							onClick={() => setOpen(true)}
-						>
-							Edit
-						</Button>
-					</DropdownMenuItem>
-					<DropdownMenuSeparator />
-					<DropdownMenuItem className="p-0">
-						<Button
-							variant="ghost"
-							className="justify-start py-0 px-2 w-full"
-							onClick={() => deleteTask(task.id)}
-						>
-							Delete
-						</Button>
-					</DropdownMenuItem>
-				</DropdownMenuContent>
-			</DropdownMenu>
-			{open ? (
-				<EditTask open={open} setOpen={setOpen} task={task} />
-			) : undefined}
-		</>
+		<Button
+			size="icon"
+			variant="ghost"
+			className="text-muted-foreground"
+			onClick={() => deleteTask(task.id)}
+		>
+			<Trash2 />
+			<span className="sr-only">delete task</span>
+		</Button>
 	)
 }
