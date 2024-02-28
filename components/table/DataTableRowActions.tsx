20f6739ceb99e5ec06acc5fb19fbe906220e2460
PaulAroo <timmypaul2006@gmail.com>
2024-02-28 17:56:25 +0100

    feat: show alert dialog before deleting task

--- components/table/DataTableRowActions.tsx
+++ components/table/DataTableRowActions.tsx
@@ -5,25 +5,48 @@ import { Task } from "@/store/types"
 import { useTaskStore } from "@/store"
 import { Button } from "@/components/ui/button"
 
-interface DataTableRowActionsProps<TData> {
-	row: Row<TData>
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+interface DataTableRowActionsProps {
+	row: Row<Task>
 }
 
-export function DataTableRowActions<TData>({
-	row,
-}: DataTableRowActionsProps<TData>) {
-	const task = row.original as Task
+export function DataTableRowActions({ row }: DataTableRowActionsProps) {
+	const task = row.original
 	const deleteTask = useTaskStore((state) => state.deleteTask)
 
 	return (
-		<Button
-			size="icon"
-			variant="ghost"
-			className="text-muted-foreground"
-			onClick={() => deleteTask(task.id)}
-		>
-			<Trash2 />
-			<span className="sr-only">delete task</span>
-		</Button>
+		<AlertDialog>
+			<AlertDialogTrigger asChild>
+				<Button size="icon" variant="ghost" className="text-muted-foreground">
+					<Trash2 />
+				</Button>
+			</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+					<AlertDialogDescription>
+						This action cannot be undone. This will permanently delete your
+						task.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+					<AlertDialogAction asChild>
+						<Button onClick={() => deleteTask(task.id)}>Delete</Button>
+					</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
 	)
 }
